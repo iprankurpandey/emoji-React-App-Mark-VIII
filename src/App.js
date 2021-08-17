3953e5864618818084c388a488d703ad55977a39
Prankur Pandey <pprankur@gmail.com>
2021-08-17 23:24:36 +0530

    updated heading and color

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./styles.css";
-// let headingText =
-let username = prompt("your name");
+import Ink from "react-ink";
+let headingText = "your name";
 let color = "purple";
 export default function App() {
   return (
     <div className="App">
-      <h1 style={{ backgroundColor: color }}> welcome {username}</h1>
+      <h1>
+        welcome <span style={{ backgroundColor: color }}> {headingText}</span>
+      </h1>
     </div>
   );
 }
