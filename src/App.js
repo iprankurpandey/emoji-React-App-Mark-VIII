56f6e52dceecda7111791086de858c69cba5336d
Prankur Pandey <pprankur@gmail.com>
2021-08-18 00:38:41 +0530

    console.log("LikeCounter", LikeCounter); outside of function

--- src/App.js
+++ src/App.js
@@ -2,27 +2,18 @@ import React, { useState } from "react";
 import { render } from "react-dom";
 import "./styles.css";
 
-const mystyle = {
-  color: "white",
-  backgroundColor: "DodgerBlue",
-  padding: "10px",
-  fontFamily: "Arial"
-};
-
 export default function App() {
   const [LikeCounter, setlikeCounter] = useState(0);
   function likeClickHandler() {
     var newLikeCounter = LikeCounter + 1;
     setlikeCounter(newLikeCounter); //confused
-    console.log("LikeCounter", LikeCounter); //process to output the value on display
+    //process to output the value on display
   }
-
+  console.log("LikeCounter", LikeCounter);
   return (
     <div className="App">
       <h1>inside out</h1>
-      <button style={mystyle} onClick={likeClickHandler}>
-        Like{" "}
-      </button>
+      <button onClick={likeClickHandler}>Like </button>
       {LikeCounter}
     </div>
   );
